58f59de6f77f8d35c0a289b1158bb3d354cff7a9
Rafael <rafael.angeles.toledano@gmail.com>
2025-04-18 16:04:19 -0300

    feat: creating user routes

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,22 +1,41 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.create(createUserDto);
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto);
   }
 
   @Patch(':id')
-  async update(
+  async updateUser(
     @Param('id') id: string,
-    @Body() updateUserDto: CreateUserDto,
+    @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.updateUser(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return this.userService.deleteUser(+id);
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<User> {
+    return this.userService.getById(Number(id));
   }
 }
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { User } from '../entities/user.entity';
+import { UpdateUserDto } from '../dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -11,12 +12,12 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
     const user = this.userRepository.create(createUserDto);
     return await this.userRepository.save(user);
   }
 
-  async update(id: number, updateUserDto: CreateUserDto): Promise<User> {
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
     await this.userRepository.update(id, updateUserDto);
     const updatedUser = await this.userRepository.findOneBy({ id });
 
@@ -25,5 +26,25 @@ export class UserService {
     }
 
     return updatedUser;
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
+    }
+
+    await this.userRepository.delete(id);
+    return user;
+  }
+
+  async getById(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Usuário com id ${id} não encontrado.`);
+    }
+    return user;
   }
 }
